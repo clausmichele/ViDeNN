4878451f629f2f0e2913be93ecd7f2fe9ff6b91a
Michele <michele@ubuntu.ubuntu-domain>
2018-10-09 09:52:10 +0200

    Images

---
+++ denoise.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo VidCNN - Learning Blind Video Denoising
+echo [*] Insert the video file name you want to denoise:
+read vidname
+mkdir noisy
+echo [*] Extracting frames to the Noisy folder...
+ffmpeg -i ./$vidname ./noisy/%04d.png
+echo [*] Denoising frames, will be stored in ./data/denoised/
+python main.py --use_gpu=1 --ckpt_dir='./ckpt_vidcnn' --save_dir='./data/denoised' --test_dir='./'
